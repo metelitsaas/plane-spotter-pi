f09bc4a70abb4b959e26a4066cf21c838c9d24f7
metelitsaas <metelitsaas@gmail.com>
2021-03-14 02:07:16 +0300

    ApiLoader abstract class added

---
+++ apps/package/utils/api_loader.py
@@ -0,0 +1,56 @@
+import datetime
+from abc import ABCMeta
+from functools import wraps
+import requests
+from requests import ReadTimeout, HTTPError
+from package.utils.logger import logger
+
+
+class ApiLoader(metaclass=ABCMeta):
+    """
+    Abstract class of API loader
+    """
+    def __init__(self, params: dict):
+        """
+        Initialization
+        :param params: web-server connection parameters
+        """
+        self._host = params['host']
+        self._port = params['port']
+
+    @staticmethod
+    def _exception_handler(function):
+        """
+        Request exception handler
+        :param function: function to wrap
+        :return: wrapped function
+        """
+        @wraps(function)
+        def wrapper(self, *method_args, **method_kwargs):
+
+            try:
+                return function(self, *method_args, **method_kwargs)
+
+            except (ReadTimeout, requests.ConnectionError, HTTPError) as error:
+                logger.warning(error)
+
+                return None
+
+            except Exception as error:
+                logger.exception(error)
+                logger.warning('Unhandled exception')
+                raise
+
+        return wrapper
+
+    @staticmethod
+    def _datetime_handler(value) -> str:
+        """
+        Wrap datatime values to ISO format
+        :param value: value to check
+        :return: wrapped value
+        """
+        if isinstance(value, datetime.datetime):
+            return value.isoformat()
+
+        return str(value)
