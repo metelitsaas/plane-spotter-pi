e74e0b44c12fca9614a8829d79fea0f7627daffc
metelitsaas <metelitsaas@gmail.com>
2021-03-28 12:36:11 +0300

    telegram-bot reconnect exception fixed and keyboard excluded

--- apps/telegram-bot/app/__main__.py
+++ apps/telegram-bot/app/__main__.py
@@ -1,7 +1,7 @@
 import os
 import telebot
+from requests.exceptions import ConnectionError
 from package.utils.logger import logger
-from answers import generate_statistics_message, generate_last_message
 from webserver_loader import WebserverLoader
 
 
@@ -19,56 +19,59 @@ bot = telebot.TeleBot(api_token)
 webserver_loader = WebserverLoader(webserver_params)
 
 
-@bot.message_handler(commands=['bot'])
-def bot_handler(message):
+@bot.message_handler(commands=['statistics'])
+def statistics(message):
     """
-    Bot keyboard handler
+    Send statistics
     :param message: received message
     """
-    keyboard = telebot.types.InlineKeyboardMarkup()
-
-    key_status = telebot.types.InlineKeyboardButton(
-        text='statistics', callback_data='statistics'
-    )
-    keyboard.add(key_status)
-
-    key_status = telebot.types.InlineKeyboardButton(
-        text='last message', callback_data='last_message'
-    )
-    keyboard.add(key_status)
-
-    bot.send_message(message.chat.id, 'Choose action:', reply_markup=keyboard)
+    answer = webserver_loader.get_statistics()
+    response = f"""
+            Statistics:
+            All messages: {answer['message_cnt']}
+            Unique planes: {answer['hex_id_dist_cnt']}
+            Emergency messages: {answer['emergency_cnt']}
+            """
+    send_message(message.chat.id, response)
 
 
-@bot.callback_query_handler(func=lambda call: True)
-def callback_worker(call):
+@bot.message_handler(commands=['last_message'])
+def last_message(message):
     """
-    Bot response logic
-    :param call: response metadata
+    Send last SBS-1 message
+    :param message: received message
     """
+    answer = webserver_loader.get_last_message()
+    response = f"""
+            Last message:
+            Plane ID: {answer['hex_id']}
+            Call sign: {answer['call_sign_nm']}
+            UTC Timestamp: {answer['creation_dttm']}
+            """
+    send_message(message.chat.id, response)
 
-    if call.data == 'statistics':
-        answer = webserver_loader.get_statistics()
-        message = generate_statistics_message(answer)
-        bot.send_message(call.message.chat.id, message)
 
-    elif call.data == 'last_message':
-        answer = webserver_loader.get_last_message()
-        message = generate_last_message(answer)
-        bot.send_message(call.message.chat.id, message)
+def send_message(chat_id, message) -> None:
+    """
+    Send message chat with exception handling
+    :param chat_id: telegram chat id
+    :param message: message body
+    """
+    while True:
+        try:
+            bot.send_message(chat_id, message)
+            break
+
+        except ConnectionError:
+            logger.warning('Connection error, trying again')
 
 
 if __name__ == '__main__':
 
-    while True:
-        try:
-            logger.info('Starting')
-            bot.polling()
+    try:
+        logger.info('Starting')
+        bot.polling()
 
-        except ConnectionResetError as error:
-            logger.warning(error)
-
-        except Exception as error:
-            logger.exception(error)
-            logger.critical('Critical exception')
-            break
+    except Exception as error:
+        logger.exception(error)
+        logger.critical('Critical exception')
